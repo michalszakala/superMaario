6a413b968a157c2f8a15ff4c625ad41bbc78f73a
Michal Szakala <MSzakal2@sk.ibm.com>
2018-01-27 22:36:16 +0100

    refresh rate adjustment

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,42 @@
 import Compositor from './Compositor.js'
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
+import { loadBackgroundSprites } from './sprites.js';
 import { loadLevel } from './loaders.js';
-import { createBackgroundLayer } from './layers.js';
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
+import Entity from './Entity.js';
+import { createMario } from './entities.js';
+import Timer from './Timer.js';
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function createSpriteLayer(sprite, pos) {
-    return function drawSpriteLayer(context) {
-        for (let i = 0; i < 20; ++i) {
-            sprite.draw('idle', context, pos.x + i * 16, pos.y);
-        }
-    }
-}
-
 Promise.all([
-    loadMarioSprite(),
+    createMario(),
     loadBackgroundSprites(),
     loadLevel('1-1')
-]).then(([marioSprite, backgroundSprites, level]) => {
+]).then(([mario, backgroundSprites, level]) => {
     
     const comp = new Compositor();
 
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
 
-    const pos = {
-        x: 0,
-        y: 0
-    };
+    const gravity = 30;
+    mario.pos.set(1, 180);
+    mario.vel.set(200, -600);
 
-    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
-    function update() {
+    const timer = new Timer(1/60);
+
+    timer.update = function update(deltaTime) {
+        
         comp.draw(context);
-        pos.x += 2;
-        pos.y += 2;
-        requestAnimationFrame(update);
+        mario.update(deltaTime);
+        mario.vel.y += gravity;
+        
     }
 
-    update();
+    timer.start();
 
 });
