ba0c72c7d46957c38950aa61e70da4c52a80f248
Michal Szakala <MSzakal2@sk.ibm.com>
2018-01-27 23:15:26 +0100

    keyboard input implementation

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,9 @@ import Entity from './Entity.js';
 import { createMario } from './entities.js';
 import Timer from './Timer.js';
 
+import Keyboard from './KeyBoardState.js';
+
+
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
@@ -20,9 +23,21 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
 
-    const gravity = 30;
+    const gravity = 2000;
     mario.pos.set(1, 180);
     mario.vel.set(200, -600);
+
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    })
+    input.listenTo(window);
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
@@ -31,9 +46,9 @@ Promise.all([
 
     timer.update = function update(deltaTime) {
         
-        comp.draw(context);
         mario.update(deltaTime);
-        mario.vel.y += gravity;
+        comp.draw(context);
+        mario.vel.y += gravity * deltaTime;
         
     }
 
